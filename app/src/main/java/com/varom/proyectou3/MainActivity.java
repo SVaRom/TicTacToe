d509bb6118bc4556c23f4be9712ae1a8bdca58a5
SVaRom <sebastian.varom13@gmail.com>
2021-11-06 21:43:53 -0600

    Tutorial Full

--- app/src/main/java/com/varom/proyectou3/MainActivity.java
+++ app/src/main/java/com/varom/proyectou3/MainActivity.java
@@ -6,15 +6,14 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button[][] gameBoard=new Button[3][3];
-    int roundC=0, playerPoints=0,cpuPoints=0;
     TextView tv1,tv2;
-    boolean playerTurn=true;
-
+    Button[][] botones=new Button[3][3];
+    Button btnReset;
+    int pPoints=0,cPoints=0,empate=0;
+    boolean pTurn=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,101 +21,118 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         tv1=findViewById(R.id.tv1);
         tv2=findViewById(R.id.tv2);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                String buttonID = "button_" + i + j;
-                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                gameBoard[i][j] = findViewById(resID);
-                gameBoard[i][j].setOnClickListener(this);
-                gameBoard[i][j].setText("");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                String  btnID="button_"+i+j;
+                int idFin=getResources().getIdentifier(btnID,"id",getPackageName());
+                botones[i][j]=findViewById(idFin);
+                botones[i][j].setOnClickListener(this);
+                botones[i][j].setText("");
             }
         }
-        Button btnReset=findViewById(R.id.btnReset);
-        btnReset.setOnClickListener(new View.OnClickListener(){
+        btnReset=findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playerPoints = 0;
-                cpuPoints = 0;
-                tv1.setText("Player: "+playerPoints);
-                tv2.setText("CPU: "+cpuPoints);
-                reset();
+                pPoints=0;
+                cPoints=0;
+                tv1.setText("Player: "+pPoints);
+                tv2.setText("CPU: "+cPoints);
+                limpiar();
             }
         });
+    }
 
+    public void limpiar(){
+        empate=0;
+        pTurn=true;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                botones[i][j].setText("");
+            }
+        }
     }
 
     @Override
     public void onClick(View view) {
-        if (((Button) view).getText().toString().equals("")){
-            if (playerTurn){
-                ((Button) view).setText("X");
-                roundC++;
-            }
-            if(verificarWin()){
-                if(playerTurn){
-                    playerPoints++;
-                    Toast.makeText(this, "Player ganó!", Toast.LENGTH_SHORT).show();
-                    tv1.setText("Player: "+playerPoints);
-                    reset();
+        if(((Button)view).getText().toString().equals("")){
+            ((Button)view).setText("O");
+            empate++;
+        }
+        if(verificar()&&pTurn){
+            pPoints++;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tv1.setText("Player: "+pPoints);
+                    limpiar();
                 }
-            }else if (roundC == 9) {
-                Toast.makeText(this, "Empate!", Toast.LENGTH_SHORT).show();
-                reset();
-            }else{
-                playerTurn=!playerTurn;
-                cpuTurn();
-            }
+            },1000);
+            tv1.setText("Player: Ganador");
+        }else if(empate==9){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tv1.setText("Player: "+pPoints);
+                    tv2.setText("CPU: "+cPoints);
+                }
+            },1000);
+            tv1.setText("Player: Empate!");
+            tv2.setText("CPU: Empate!");
+            limpiar();
+        }else{
+            pTurn=!pTurn;
+            cpu();
         }
     }
-    public boolean verificarWin(){
-        return gameBoard[0][0].getText() == gameBoard[0][1].getText() && gameBoard[0][0].getText() == gameBoard[0][2].getText() && gameBoard[0][0].getText() != ""
-                || gameBoard[1][0].getText() == gameBoard[1][1].getText() && gameBoard[1][0].getText() == gameBoard[1][2].getText() && gameBoard[1][0].getText() != ""
-                || gameBoard[2][0].getText() == gameBoard[2][1].getText() && gameBoard[2][0].getText() == gameBoard[2][2].getText() && gameBoard[2][0].getText() != ""
-                || gameBoard[0][0].getText() == gameBoard[1][0].getText() && gameBoard[0][0].getText() == gameBoard[2][0].getText() && gameBoard[0][0].getText() != ""
-                || gameBoard[0][1].getText() == gameBoard[1][1].getText() && gameBoard[0][1].getText() == gameBoard[2][1].getText() && gameBoard[0][1].getText() != ""
-                || gameBoard[0][2].getText() == gameBoard[1][2].getText() && gameBoard[0][2].getText() == gameBoard[2][2].getText() && gameBoard[0][2].getText() != ""
-                || gameBoard[0][0].getText() == gameBoard[1][1].getText() && gameBoard[0][0].getText() == gameBoard[2][2].getText() && gameBoard[0][0].getText() != ""
-                || gameBoard[0][2].getText() == gameBoard[1][1].getText() && gameBoard[0][2].getText() == gameBoard[2][0].getText() && gameBoard[0][2].getText() != "";
+
+    public boolean verificar(){
+        return botones[0][0].getText() == botones[0][1].getText() && botones[0][0].getText() == botones[0][2].getText() && botones[0][0].getText() != ""
+                || botones[1][0].getText() == botones[1][1].getText() && botones[1][0].getText() == botones[1][2].getText() && botones[1][0].getText() != ""
+                || botones[2][0].getText() == botones[2][1].getText() && botones[2][0].getText() == botones[2][2].getText() && botones[2][0].getText() != ""
+                || botones[0][0].getText() == botones[1][0].getText() && botones[0][0].getText() == botones[2][0].getText() && botones[0][0].getText() != ""
+                || botones[0][1].getText() == botones[1][1].getText() && botones[0][1].getText() == botones[2][1].getText() && botones[0][1].getText() != ""
+                || botones[0][2].getText() == botones[1][2].getText() && botones[0][2].getText() == botones[2][2].getText() && botones[0][2].getText() != ""
+                || botones[0][0].getText() == botones[1][1].getText() && botones[0][0].getText() == botones[2][2].getText() && botones[0][0].getText() != ""
+                || botones[0][2].getText() == botones[1][1].getText() && botones[0][2].getText() == botones[2][0].getText() && botones[0][2].getText() != "";
     }
-    public void cpuTurn(){
+
+    public void cpu(){
         Random random = new Random();
         int i,j;
         do {
             i = random.nextInt(3);
             j = random.nextInt(3);
-        } while (gameBoard[i][j].getText() != "");
+        } while (botones[i][j].getText() != "");
         final int fI=i,fJ=j;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                gameBoard[fI][fJ].setText("O");
-                roundC++;
-                if(verificarWin()){
-                    if(!playerTurn){
-                        cpuPoints++;
-                        Toast.makeText(getApplicationContext(), "CPU ganó!", Toast.LENGTH_SHORT).show();
-                        tv2.setText("CPU: "+cpuPoints);
-                        reset();
-                    }
-                }else if (roundC == 9) {
-                    Toast.makeText(getApplicationContext(), "Empate!", Toast.LENGTH_SHORT).show();
-                    reset();
-                }else playerTurn=!playerTurn;
+                botones[fI][fJ].setText("X");
+                empate++;
+                if(verificar()&&!pTurn){
+                    cPoints++;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv2.setText("CPU: "+cPoints);
+                            limpiar();
+                        }
+                    },1000);
+                    tv2.setText("CPU: Ganador");
+                }else if (empate == 9) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv1.setText("Player: "+pPoints);
+                            tv2.setText("CPU: "+cPoints);
+                        }
+                    },1000);
+                    tv1.setText("Player: Empate!");
+                    tv2.setText("CPU: Empate!");
+                    limpiar();
+                }else pTurn=!pTurn;
             }
         }, 500);
-    }
-    public void reset(){
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        gameBoard[i][j].setText("");
-                    }
-                }
-                roundC = 0;
-                playerTurn = true;
-            }
-        }, 1000);
     }
 }
